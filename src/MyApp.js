ba34a355665cde08b42c994f8d4dc49774cd59fe
jdohun <wjdehgns97@naver.com>
2024-01-16 16:39:36 +0900

    feat: create MyApp

---
+++ src/MyApp.js
@@ -0,0 +1,17 @@
+
+function MyButton() {
+    return (
+        <button>
+            I'm a button
+        </button>
+    );
+}
+
+ export function MyApp() {
+    return (
+        <div>
+            <h1>Welcome to my app</h1>
+            <MyButton/>
+        </div>
+    );
+}
