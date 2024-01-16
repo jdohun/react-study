a585b29e572b65550119fafbd046e7b4abdfd60c
jdohun <wjdehgns97@naver.com>
2024-01-16 17:46:20 +0900

    feat: writing markup with JSX

--- src/MyApp.js
+++ src/MyApp.js
@@ -1,10 +1,4 @@
-function MyButton() {
-    return (
-        <button>
-            I'm a button
-        </button>
-    );
-}
+import "./MyCss.css";
 
 export function MyApp() {
     return (
@@ -12,7 +6,16 @@ export function MyApp() {
             <h1>Welcome to my app</h1>
             <MyButton/>
             <AboutPage/>
+            <Profile/>
         </div>
+    );
+}
+
+function MyButton() {
+    return (
+        <button>
+            I'm a button
+        </button>
     );
 }
 
@@ -22,6 +25,29 @@ function AboutPage() {
         <>
             <h1>About</h1>
             <p>Hello there.<br/>How do you do?</p>
+        </>
+    );
+}
+
+const user = {
+    name: 'Hedy Lamarr',
+    imageUrl: 'https://i.imgur.com/yXOvdOSs.jpg',
+    imageSize: 90,
+};
+
+function Profile() {
+    return (
+        <>
+            <h1>{user.name}</h1>
+            <img
+                className="avatar"
+                src={user.imageUrl}
+                alt={'Photo of ' + user.name}
+                style={{
+                    width: user.imageSize,
+                    height: user.imageSize
+                }}
+            />
         </>
     );
 }
