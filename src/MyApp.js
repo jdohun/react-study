e84d509faed2cbe02bf1a03d669fe5e7f4cb71e4
jdohun <wjdehgns97@naver.com>
2024-01-16 17:52:42 +0900

    feat: Rendering lists

--- src/MyApp.js
+++ src/MyApp.js
@@ -7,6 +7,7 @@ export function MyApp() {
             <MyButton/>
             <AboutPage/>
             <Profile/>
+            <ShoppingList/>
         </div>
     );
 }
@@ -49,5 +50,28 @@ function Profile() {
                 }}
             />
         </>
+    );
+}
+
+const products = [
+    {title: 'Cabbage', isFruit: false, id: 1},
+    {title: 'Garlic', isFruit: false, id: 2},
+    {title: 'Apple', isFruit: true, id: 3},
+];
+
+const listItems = products.map(product =>
+    <li
+        key={product.id}
+        style={{
+            color: product.isFruit ? 'magenta' : 'darkgreen'
+        }}
+    >
+        {product.title}
+    </li>
+);
+
+function ShoppingList() {
+    return (
+        <ul>{listItems}</ul>
     );
 }
